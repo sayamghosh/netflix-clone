987991f9902e1e523be1f77500b8bc662d036d01
Sayam Ghosh <sayamghosh.dev@gmail.com>
2025-01-19 15:44:50 +0530

    signup page created

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Input } from "@/components/ui/input"
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { FcGoogle } from "react-icons/fc";
+import { SiGithub } from "react-icons/si";
+
+export default function Page() {
+  return (
+    <div className='bg-black/90 py-6 px-6 rounded w-[20rem] mx-auto h-fit'>
+      <form className='space-y-3'>
+        <h2 className='text-xl'>Login</h2>
+        <Input type="email" placeholder="Email" className='bg-[#333] placeholder:text-xs placeholder:text-gray-400  w-full' />
+        <Input type="password" placeholder="Password" className='bg-[#333] placeholder:text-xs placeholder:text-gray-400 w-full ' />
+        <Button className='w-full bg-[#e50914] hover:bg-red-800 text-white font-semibold'>Login</Button>
+      </form>
+      <div className='text-sm text-gray-600 mt-2'>
+      Already have an account?<Link href={'/login'} className='text-white hover:underline ml-1'> Login now!</Link>
+      </div>
+      <div className='w-full items-center justify-center flex mt-4 gap-2'>
+        <Button size="icon" variant={"outline"}><FcGoogle /></Button>
+        <Button size="icon" variant={"outline"}><SiGithub /></Button>
+      </div>
+    </div>
+  )
+}
